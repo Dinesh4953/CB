ec905566d3308d25ead176ec7c5b3461701aac06
Dinesh4953 <dinu08642@gmail.com>
2025-10-18 23:07:51 +0530

    commit

--- campus_bridge/settings.py
+++ campus_bridge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-1zd-()-(7g%ujzdsq+xrh5#p@$ahp**u)w3i81q1h_6&!xrmi3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cb-keoh.onrender.com", "localhost", "127.0.0.1", "*"]
 
 
 # Application definition
